69ad33cf27f7bcb881486f70f306aba97a5cb783
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-08 18:07:33 +0530

    app is completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const typingChangeHandler = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="App">
-      <input type="text" placeholder="search..." />
-      {JSONDATA.map((val, key) => {
+      <input type="text" placeholder="search..." onChange={typingChangeHandler}/>
+      {JSONDATA.filter((val) => {
+        if(searchTerm === ""){
+          return val;
+        }else if(val.first_name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+          return val;
+        }
+      }).map((val, key) => {
         return (
           <div className="user">
             <p>{val.first_name}</p>
